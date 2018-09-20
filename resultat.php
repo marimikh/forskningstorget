419a3bee0b839f42f22716236d8e558de5f83d7c
marimikh <you@example.com>
2018-09-20 23:59:52 +0200

    resultant page

--- resultat.php
+++ resultat.php
@@ -16,12 +16,13 @@ $_SESSION['score_final'] = $_SESSION['score2']+ $_POST['question1'] + $_POST['qu
 	<?php 
 	if (isset($_POST['finish'])){
         if ($_SESSION['score_final']<12) {
-			echo "<i>Du ble den uforsiktige apekatten. Det er ikke alltid bra å forhaste ting, 
+			echo "<h1>Du ble den uforsiktige<br> apekatten</h1> 
+			<br><i>Det er ikke alltid bra å forhaste ting, 
 			tenk gjerne en gang eller to, før du legger noe ut på internett. Husk at det er fort gjort 
 			å komme borti upassende innhold eller ubehagelige personer. Vær smart når du bruker internett.</i>";
             echo "<img id='innerImg' src='img/gazelle.jpg'/>";
         }
-        elseif($_SESSION['score_final']>=12 && $_SESSION['score_final']<21){
+        elseif($_SESSION['score_final']>=12 && $_SESSION['score_final']<16){
 			echo "<h1>Du ble den forsiktige<br> koala bjørnen</h1> 
 			<br><i>Det er ikke alltid du 
 			vet hva som er rett og galt og det hender at du ofte setter deg i skyggen.
@@ -29,10 +30,10 @@ $_SESSION['score_final'] = $_SESSION['score2']+ $_POST['question1'] + $_POST['qu
 			 godt å spørre om hjelp eller råd om du er i tvil.</i>";
             echo "<img id='innerImg' src='img/monkey.jpg'/>";
         }
-		else { echo "Du ble den vise elefanten. Du vet akkurat 
-			hvordan du skal oppføre deg på internett og vet hvor langt grensene går. 
+		else { echo "<h1>Du ble den vise elefanten</h1> 
+			<br><i>Du vet akkurat hvordan du skal oppføre deg på internett og vet hvor langt grensene går. 
 			Du er en som tenker deg godt igjennom før du gjør en handling og passer på at de rundt deg har det behagelig. 
-			Fortsett å vær god som du er!";
+			Fortsett å vær god som du er!</i>";
                echo "<img id='innerImg' src='img/monkey2.jpg'/>";
             }
 	}		
